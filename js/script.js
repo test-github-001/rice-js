b123cf9e6f12f52a165c2ca4298d76342a9ef7d8
test-github-001 <107862407+test-github-001@users.noreply.github.com>
2023-02-09 12:21:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,17 @@ const roadWidth = road.clientWidth;
 const carWidth = 100;
 const screenOffset = 50;
 
+// определения старта и финиша на экране в пикселях
 const start = screenOffset;
 const finish = roadWidth - carWidth - screenOffset;
 
+// переменная, определяющая победителя
+let winner;
+
+// ускорение автомобиля с шансам в 30% до 0,01 (пиксель за шаг)
+const getAcceleration = () => (Math.random() < 0.7) ? 0 : 0.01;
+
+// добавление красного автомобиля на стартовую позицию
 const carRedDiv = document.createElement('img');
 carRedDiv.src = './src/images/car_red.png';
 carRedDiv.className = 'car';
@@ -17,6 +25,7 @@ carRedDiv.style.left = start + 'px';
 carRedDiv.style.bottom = 160 + 'px';
 road.append( carRedDiv );
 
+// carGreenDiv
 const carGreenDiv = document.createElement('img');
 carGreenDiv.src = './src/images/car_green.png';
 carGreenDiv.className = 'car';
@@ -24,6 +33,7 @@ carGreenDiv.style.left = start + 'px';
 carGreenDiv.style.bottom = 110 + 'px';
 road.append( carGreenDiv );
 
+// carBlueDiv
 const carBlueDiv = document.createElement('img');
 carBlueDiv.src = './src/images/car_blue.png';
 carBlueDiv.className = 'car';
@@ -31,6 +41,7 @@ carBlueDiv.style.left = start + 'px';
 carBlueDiv.style.bottom = 60 + 'px';
 road.append( carBlueDiv );
 
+// carYellowDiv
 const carYellowDiv = document.createElement('img');
 carYellowDiv.src = './src/images/car_yellow.png';
 carYellowDiv.className = 'car';
@@ -38,12 +49,9 @@ carYellowDiv.style.left = start + 'px';
 carYellowDiv.style.bottom = 10 + 'px';
 road.append( carYellowDiv );
 
-let winner;
-
-const getAcceleration = () => (Math.random() < 0.7) ? 0 : 0.01;
-
+// описание объекта красного автомобиля
 const carRed = {
-    name: 'RED CAR',
+    name: 'КРАСНЫЙ ОХОТНИК',
     img: carRedDiv,
     speed: 0,
     dist: start,
@@ -56,7 +64,7 @@ const carRed = {
 }
 
 const carGreen = {
-    name: 'GREEN CAR',
+    name: 'ЗЕЛЕНАЯ КОБРА',
     img: carGreenDiv,
     speed: 0,
     dist: start,
@@ -69,7 +77,7 @@ const carGreen = {
 }
 
 const carBlue = {
-    name: 'BLUE CAR',
+    name: 'СИНИЙ ПРИЗРАК',
     img: carBlueDiv,
     speed: 0,
     dist: start,
@@ -82,7 +90,7 @@ const carBlue = {
 }
 
 const carYellow = {
-    name: 'YELLOW CAR',
+    name: 'ЖЕЛТЫЙ СКОРПИОН',
     img: carYellowDiv,
     speed: 0,
     dist: start,
@@ -94,21 +102,55 @@ const carYellow = {
     }
 }
 
-let riceLoop = setInterval( () => {
-    let maxDist = Math.max(carRed.dist, carGreen.dist, carBlue.dist, carYellow.dist);
-    if (maxDist >= finish) {
-        clearInterval(riceLoop);
-        switch (maxDist) {
-            case carRed.dist    : winner = carRed.name; break;
-            case carGreen.dist  : winner = carGreen.name; break;
-            case carBlue.dist   : winner = carBlue.name; break;
-            case carYellow.dist : winner = carYellow.name; break;
-        }
-        info.innerHTML = 'WINNER is ' + winner;
-    } else {
-        carRed.move();
-        carGreen.move();
-        carBlue.move();
-        carYellow.move();
+// запуск гонки
+info.innerHTML = 'Для старта гонки кликните по экрану';
+
+let isStartRice = false;
+document.body.onclick = function() {
+    if (!isStartRice) {
+        isStartRice = true;
+        carRedDiv.style.left = start + 'px';
+        carGreenDiv.style.left = start + 'px';
+        carBlueDiv.style.left = start + 'px';
+        carYellowDiv.style.left = start + 'px';
+        carRed.speed = 0;
+        carRed.dist = start;
+        carGreen.speed = 0;
+        carGreen.dist = start;
+        carBlue.speed = 0;
+        carBlue.dist = start;
+        carYellow.speed = 0;
+        carYellow.dist = start;
+        info.innerHTML =                   '-=3=-'       ;
+        setTimeout( () => info.innerHTML = ' -2- ', 1000);
+        setTimeout( () => info.innerHTML = '  1  ', 2000);
+        setTimeout( () => {
+            info.innerHTML =               'СТАРТ'       ; 
+            startRice();
+        }, 3000);
     }
-}, 7);
+}
+
+function startRice() {
+    // ускарение автомобилей с интервалом 7 миллисекунд, пока не выявлен победитель
+    let riceLoop = setInterval( () => {
+        let maxDist = Math.max(carRed.dist, carGreen.dist, carBlue.dist, carYellow.dist);
+        if (maxDist >= finish) {
+            clearInterval(riceLoop);
+            switch (maxDist) {
+                case carRed.dist    : winner = carRed.name; break;
+                case carGreen.dist  : winner = carGreen.name; break;
+                case carBlue.dist   : winner = carBlue.name; break;
+                case carYellow.dist : winner = carYellow.name; break;
+            }
+            info.innerHTML = `Победитель - <b>${winner}<b>!`;
+            winner = undefined;
+            isStartRice = false;
+        } else {
+            carRed.move();
+            carGreen.move();
+            carBlue.move();
+            carYellow.move();
+        }
+    }, 7);
+}
